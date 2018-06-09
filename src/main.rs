f731cd6cdb3e723ee8e328f11caf83004a5a376d
Nat Quayle Nelson <natquaylenelson@gmail.com>
2018-06-09 17:51:46 -0400

    Old changes

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 extern crate magick_rust;
-use magick_rust::{MagickWand, magick_wand_genesis};
+use magick_rust::{MagickWand, PixelWand, magick_wand_genesis};
 use std::sync::{Once, ONCE_INIT};
 
 // Used to make sure MagickWand is initialized exactly once. Note that we
@@ -10,6 +10,12 @@ fn main() {
     START.call_once(|| {
         magick_wand_genesis();
     });
+
+
+    // Standard printers and paper will be 300dpi printed on 8.5 x 11.
+    let DPI: usize = 300;
+    let PAPER_WIDTH: usize = DPI * 11;
+    let PAPER_HEIGHT: usize = (DPI as f32 *8.5) as usize;
 
 
     // Open 8 pages
@@ -24,7 +30,7 @@ fn main() {
         println!("{}",file);
         top_wand.read_image(file.as_str()).expect("failed to read this");
         top_wand.flop_image().expect("Failed to flop??"); 
-        top_wand.flip_image().expect("Failed to flop??"); 
+        top_wand.flip_image().expect("Failed to flip??"); 
     }
 
     for page in bottom_sequence.iter() {
@@ -36,8 +42,13 @@ fn main() {
     let mut top_wand = top_wand.append_all(false);
     let bottom_wand = bottom_wand.append_all(false);
 
-    top_wand.add_image(&bottom_wand);
+    top_wand.add_image(&bottom_wand).expect("failed to add bottom to top");
     let all_wand = top_wand.append_all(true);
+
+    all_wand.adaptive_resize_image(PAPER_WIDTH, PAPER_HEIGHT).expect("Failed to resize image");
+
+    let background = PixelWand::new();
+    all_wand.rotate_image(&background, 90.0).expect("failed to rotate");
 
     all_wand.write_image("zine.jpg").expect("failed to write this");
 }
